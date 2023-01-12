1807daa1147859c66863028a20841c4288e24afe
Sue <100839777+sue-creator@users.noreply.github.com>
2023-01-12 19:34:09 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;    
+    answer = num_list;
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
