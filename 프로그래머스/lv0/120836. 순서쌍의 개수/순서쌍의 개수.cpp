e16d3fed832034f1e33ffd14e3b50de1d1e49f91
Sue <100839777+sue-creator@users.noreply.github.com>
2023-01-14 11:17:39 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int solution(int n){
+    int answer = 0;
+    int m=int(sqrt(double(n)));
+
+    for(int i=1; i<=m; i++){
+        if(n%i==0) answer+=2;}
+
+    if(m*m==n) answer--;
+
+    return answer;
+}
