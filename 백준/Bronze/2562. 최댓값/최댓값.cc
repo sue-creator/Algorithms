fd05137bbaec4ff2af63aa9b183c68fc87e216d7
Sue <100839777+sue-creator@users.noreply.github.com>
2023-01-18 04:19:37 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, const char *argv[]) {
+	int maxValue = -1;
+	int maxValueIndex;
+	int num;		
+	for (int i = 0; i < 9; i++) {
+		cin >> num;
+
+		if (num > maxValue) {
+			maxValue = num;
+			maxValueIndex = i;
+		}
+	}
+	cout << maxValue << "\n";
+	cout << maxValueIndex + 1;
+	return 0;
+}
