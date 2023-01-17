c117da9eacffa6656610651d3ae4f89fd7b02ac9
Sue <100839777+sue-creator@users.noreply.github.com>
2023-01-18 00:37:47 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n=1;
+    cin >> n;
+    int dp[n+1];
+    dp[0]=1;
+    
+    for(int i=1; i<n+1; i++){
+        dp[i] = i*dp[i-1];
+        }
+    
+    printf("%d", dp[n]);
+    
+    return 0;
+}
