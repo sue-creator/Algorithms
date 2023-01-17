73940828f184b3819a2de79dea6056332d745604
Sue <100839777+sue-creator@users.noreply.github.com>
2023-01-18 03:36:03 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n, m = map(int, input().split())
+maps = []
+for _ in range(n):
+    maps.append(list(map(int, input())))
+
+answer = 0
+n = len(maps)
+m = len(maps[-1])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if (nx < 0 or nx >= n or ny < 0 or ny >= m): continue
+            if maps[nx][ny] == 0:  continue
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                queue.append((nx, ny))
+
+    return maps[n-1][m-1]
+
+ans = bfs(0, 0)
+print(ans)
