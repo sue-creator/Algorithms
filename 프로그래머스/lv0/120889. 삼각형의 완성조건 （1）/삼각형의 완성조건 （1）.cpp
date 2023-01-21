b828c63bffd9204af601d8e78eeafde80e1a7e71
Sue <100839777+sue-creator@users.noreply.github.com>
2023-01-22 02:38:05 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> sides) {
+    sort(sides.begin(),sides.end());
+    if (sides[0]+sides[1] <= sides[2]){
+        return 2;
+    }
+    else return 1;
+}
