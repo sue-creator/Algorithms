3e12ce4be1c3912915f5cc28658bf54cc914e765
Sue <100839777+sue-creator@users.noreply.github.com>
2023-03-13 16:01:02 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -0,0 +1,11 @@
+def solution(order):
+    answer = 0
+    order = str(order)
+
+    for i in range(len(order)):
+        if order[i] == "3" or order[i] == "6" or order[i] == "9":
+            answer += 1
+        else:
+            continue
+            
+    return answer
